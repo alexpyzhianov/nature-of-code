7f1b06999a99b6e74085f8a03a1a41a6c6a1d144
Alexey Pyzhianov <alex.pyzhianov@gmail.com>
2020-12-16 10:43:19 +0300

    Improvements to friction

--- examples/22-friction/src/index.ts
+++ examples/22-friction/src/index.ts
@@ -6,9 +6,9 @@ interface Bullet {
 }
 
 const sketch = (p: P5) => {
-    const bulletsCount = 1000;
-    const planesCount = 21;
-    const bulletsPerPlane = bulletsCount / planesCount;
+    const bulletsPerPlane = 20;
+    const planesCount = 20;
+    const bulletsCount = planesCount * bulletsPerPlane;
 
     const canvasWidth = p.windowWidth;
     const canvasHeight = p.windowHeight;
@@ -18,22 +18,32 @@ const sketch = (p: P5) => {
         .map((_, i) => {
             const angleA = (p.TWO_PI / bulletsPerPlane) * (i % bulletsPerPlane);
             const angleB =
-                (p.TWO_PI / planesCount) * Math.floor(i / bulletsPerPlane);
+                (p.PI / planesCount) * Math.floor(i / bulletsPerPlane);
 
             return {
-                position: p
-                    .createVector(
-                        Math.cos(angleB) * Math.cos(angleA),
-                        Math.sin(angleB) * Math.cos(angleA),
-                        Math.sin(angleA),
-                    )
-                    .mult(100),
+                position: p.createVector(0, 0).mult(100),
                 velocity: p.createVector(0, 0),
             };
         });
 
     p.setup = function () {
         p.createCanvas(canvasWidth, canvasHeight, p.WEBGL);
+
+        p.stroke(150);
+        p.debugMode(100, 10, 0, 0, 0, 20, 0, -40, 0);
+        p.noStroke();
+
+        p.camera(
+            Math.cos(p.frameCount * 0.01) * 100,
+            Math.sin(-p.frameCount * 0.005) * 1000,
+            Math.sin(p.frameCount * 0.01) * 1000,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+        );
 
         p.background(0);
         p.fill(255, 100);
@@ -42,18 +52,7 @@ const sketch = (p: P5) => {
 
     p.draw = function () {
         p.background(0);
-
-        p.camera(
-            Math.cos(p.frameCount * 0.01) * 1500,
-            Math.sin(-p.frameCount * 0.005) * 1500,
-            Math.sin(p.frameCount * 0.01) * 1500,
-            0,
-            0,
-            750,
-            0,
-            1,
-            0,
-        );
+        p.orbitControl();
 
         bullets.forEach((bullet) => {
             p.sphere(10, 10, 10).translate(
