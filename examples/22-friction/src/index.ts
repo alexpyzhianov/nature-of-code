d40f704ce5741c3fb943319dcffc3179056da624
Alexey Pyzhianov <alex.pyzhianov@gmail.com>
2020-12-25 00:14:56 +0300

    Friction proof of concept

--- examples/22-friction/src/index.ts
+++ examples/22-friction/src/index.ts
@@ -3,40 +3,53 @@ import P5 from "p5";
 interface Bullet {
     position: P5.Vector;
     velocity: P5.Vector;
+    mass: number;
 }
 
 const sketch = (p: P5) => {
-    const bulletsPerPlane = 20;
-    const planesCount = 20;
-    const bulletsCount = planesCount * bulletsPerPlane;
+    const side = 10;
+    const bulletsCount = p.pow(side, 3);
 
     const canvasWidth = p.windowWidth;
     const canvasHeight = p.windowHeight;
-
-    const bullets: Bullet[] = Array(bulletsCount)
-        .fill(undefined)
-        .map((_, i) => {
-            const angleA = (p.TWO_PI / bulletsPerPlane) * (i % bulletsPerPlane);
-            const angleB =
-                (p.PI / planesCount) * Math.floor(i / bulletsPerPlane);
-
-            return {
-                position: p.createVector(0, 0).mult(100),
-                velocity: p.createVector(0, 0),
-            };
-        });
+    let bullets: Bullet[];
 
     p.setup = function () {
         p.createCanvas(canvasWidth, canvasHeight, p.WEBGL);
 
-        p.stroke(150);
-        p.debugMode(100, 10, 0, 0, 0, 20, 0, -40, 0);
-        p.noStroke();
+        bullets = Array(bulletsCount)
+            .fill(undefined)
+            .map((_, i) => {
+                const slice = Math.floor(i / p.pow(side, 2));
+                const iter = i % p.pow(side, 2);
+                const row = iter % side;
+                const col = Math.floor(iter / side);
 
+                const orientation = p.createVector(
+                    row - side / 2,
+                    col - side / 2,
+                    slice - side / 2,
+                );
+
+                return {
+                    position: orientation.copy().mult(10),
+                    velocity: orientation.copy().mult(2),
+                    mass: p.randomGaussian(5, 5),
+                };
+            });
+
+        console.log(bullets);
+
+        p.noStroke();
+        p.background(0);
+    };
+
+    p.draw = function () {
+        p.background(0);
         p.camera(
-            Math.cos(p.frameCount * 0.01) * 100,
-            Math.sin(-p.frameCount * 0.005) * 1000,
-            Math.sin(p.frameCount * 0.01) * 1000,
+            Math.cos(p.frameCount / 100) * 500,
+            Math.sin(p.frameCount / 400) * 500,
+            Math.sin(p.frameCount / 200) * 500,
             0,
             0,
             0,
@@ -45,21 +58,24 @@ const sketch = (p: P5) => {
             0,
         );
 
-        p.background(0);
-        p.fill(255, 100);
-        p.noStroke();
-    };
-
-    p.draw = function () {
-        p.background(0);
-        p.orbitControl();
-
         bullets.forEach((bullet) => {
-            p.sphere(10, 10, 10).translate(
+            p.push();
+            p.translate(
                 bullet.position.x,
                 bullet.position.y,
                 bullet.position.z,
             );
+            p.fill(bullet.mass * 100, 0, 0, 120);
+            p.sphere(3);
+            p.pop();
+
+            bullet.position.add(bullet.velocity);
+
+            if (bullet.velocity.mag() > 0.001) {
+                const friction = bullet.velocity.copy().div(bullet.mass);
+
+                bullet.velocity.sub(friction);
+            }
         });
     };
 };
